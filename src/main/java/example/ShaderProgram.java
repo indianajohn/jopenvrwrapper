5b660d3d056f6bec4501606453d7c101a9d79688
John <johnstalbaum@gmail.com>
2016-07-31 01:19:17 -0700

    Implemented code for setting uniforms.

--- src/main/java/example/ShaderProgram.java
+++ src/main/java/example/ShaderProgram.java
@@ -2,7 +2,10 @@ package example;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.FloatBuffer;
 
+import org.joml.Matrix4f;
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 import static org.lwjgl.opengl.GL20.*;
@@ -12,6 +15,7 @@ public class ShaderProgram
     // OpenGL handle that will point to the executable shader program
     // that can later be used for rendering
     private int programId;
+    protected static FloatBuffer buf16Pool;
 
     public void init(String vertexShaderFilename, String fragmentShaderFilename)
     {
@@ -104,9 +108,82 @@ public class ShaderProgram
         return vertexCode.toString();
     }
 
+    /**
+     * Gets the location of the specified uniform name.
+     * @param str the name of the uniform
+     * @return the location of the uniform in this program
+     */
+    public int getUniformLocation(String str) {
+        return glGetUniformLocation(programId, str);
+    }
+
 
     public int getProgramId() {
         return programId;
+    }
+
+    /**
+     * Sets the uniform data at the specified location (the uniform type may be int, bool or sampler2D).
+     * @param loc the location of the int/bool/sampler2D uniform
+     * @param i the value to set
+     */
+    public void setUniformi(int loc, int i) {
+        if (loc==-1) return;
+        glUniform1i(loc, i);
+    }
+
+    public void setUniformf(int loc, float f) {
+        if (loc==-1) return;
+        glUniform1f(loc, f);
+    }
+
+    /**
+     * Sends a 4x4 matrix to the shader program.
+     * @param loc the location of the mat4 uniform
+     * @param transposed whether the matrix should be transposed
+     * @param mat the matrix to send
+     */
+    public void setUniformMatrix(int loc, boolean transposed, Matrix4f mat) {
+        if (loc==-1) return;
+        if (buf16Pool == null)
+            buf16Pool = BufferUtils.createFloatBuffer(16);
+        buf16Pool.clear();
+        mat.get(buf16Pool);
+        buf16Pool.flip();
+        glUniformMatrix4fv(loc, transposed, buf16Pool);
+    }
+
+    public boolean setUniformi(String name, int i) {
+        int loc = getUniformLocation(name);
+        if (loc >= 0)
+            setUniformi(loc,i);
+        else {
+            System.out.println("Warning: tried to set an invalid integer uniform.");
+            return false;
+        }
+        return true;
+    }
+    public boolean setUniformf(String name, float f) {
+        int loc = getUniformLocation(name);
+        if (loc >= 0)
+            setUniformf(loc,f);
+        else {
+            System.out.println("Warning: tried to set an invalid float uniform.");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean setUniformMatrix(String name, boolean transposed, Matrix4f mat) {
+        int loc = getUniformLocation(name);
+        if (loc >= 0) {
+            setUniformMatrix(loc,transposed,mat);
+        }
+        else {
+            System.out.println("Warning: tried to set an invalid uniform matrix.");
+            return false;
+        }
+        return true;
     }
 }
 
